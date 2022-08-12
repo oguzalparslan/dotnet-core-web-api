8e3ace4bcc2e458f375ecca5bbd402fa1996cbdd
oguzalparslan <oguzalparsln@icloud.com>
2022-08-12 14:29:49 +0300

    Deneme Commit

--- myApp.Service/Service/Service.cs
+++ myApp.Service/Service/Service.cs
@@ -2,11 +2,6 @@
 using myApp.Core.Repositories;
 using myApp.Core.Services;
 using myApp.Core.UnitOfWork;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace myApp.Service.Service
 {
